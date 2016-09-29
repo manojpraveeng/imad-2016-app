69eee9c2dd397d14f5f6c87bff658460dda17ab2
manojpraveeng <manojpraveeng@gmail.com>
2016-09-29 13:43:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,23 @@
 //counter code
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function () {
-    counter = counter + 1;
-    var span = document.getElementById('count');
+
+var request = new XMLHttpRequest();
+
+request.onreadystatechange=function(){
+    if (request.steadyState===XMLHttpRequest.DONE) {
+        if (request.status===200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
     span.innerHTML = counter.toString();
+        }
+    }
+};
+
+request.open('GET','manojpraveeng.imad.hasura-app.io/counter', true);
+request.send(null);
+
+    
 };
   
 
